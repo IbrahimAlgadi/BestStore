41a2f042ef2a3c184b0a4a7b51cc4904b129e280
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-07-08 22:29:13 +0400

    Add project files.

---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            // Call it options and pass it to the base class
+        }
+
+    }
+}
