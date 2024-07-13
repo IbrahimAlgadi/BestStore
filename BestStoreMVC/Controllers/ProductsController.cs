eeddd3a92fe77ba0f7081c4a25c34e5438758cb9
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-07-13 22:04:44 +0400

    Post Route Add Save Image File Logic

--- BestStoreMVC/Controllers/ProductsController.cs
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using BestStoreMVC.Services;
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BestStoreMVC.Controllers
@@ -6,16 +7,49 @@ namespace BestStoreMVC.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment webEenv;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment webEenv)
         {
             this.context = context;
-            
+            this.webEenv = webEenv;
         }
         public IActionResult Index()
         {
             var products = context.Products.OrderByDescending(p => p.Id).ToList();
             return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+
+            if (productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+            // Save Image File
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile?.FileName);
+
+            string imageFullPath = webEenv.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            return RedirectToAction("Index", "Products");
         }
     }
 }
--- BestStoreMVC/Models/ProductDto.cs
+++ BestStoreMVC/Models/ProductDto.cs
@@ -6,19 +6,19 @@ namespace BestStoreMVC.Models
     {
 
         [Required, MaxLength(100)]
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
 
         [Required, MaxLength(100)]
-        public string Brand { get; set; }
+        public string Brand { get; set; } = "";
 
         [Required, MaxLength(100)]
-        public string Category { get; set; }
+        public string Category { get; set; } = "";
 
         [Required]
         public decimal Price { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string Description { get; set; } = "";
 
         public IFormFile? ImageFile { get; set; }
 
