0cfb2dfaa4c7a8ee995b00d57478c830d887f4b7
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-07-14 16:48:08 +0400

    Display Product Details

--- BestStoreMVC/Controllers/StoreController.cs
+++ BestStoreMVC/Controllers/StoreController.cs
@@ -1,24 +1,89 @@
-﻿using BestStoreMVC.Services;
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BestStoreMVC.Controllers
 {
     public class StoreController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly int pageSize = 4;
 
         public StoreController(ApplicationDbContext context)
         {
             this.context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex, string? search, string? brand, string? category, string? sort)
         {
-            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+
+            IQueryable<Product> query = context.Products;
+
+
+            // search functionality
+            if (search != null)
+            {
+                search = search.Trim();
+                Console.WriteLine("Search Value: " + search);
+                query = query.Where(p => p.Name.Contains(search) || p.Brand.Contains(search));
+            }
+
+            // ordering
+            // sort functionality
+            if (sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.Price);
+            }
+            else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.Price);
+            }
+            else
+            {
+                query = query.OrderByDescending(p => p.Id);
+            }
+
+
+            // pagination
+            if (pageIndex < 1)
+                pageIndex = 1;
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            // list records
+            var products = query.ToList();
 
             ViewBag.Products = products;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
 
-            return View(products);
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Brand = brand,
+                Category = category,
+                Sort = sort,
+            };
+
+            return View(storeSearchModel);
         }
+
+
+        public IActionResult Details(int id) 
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Store", "Index");
+            }
+
+            return View(product);
+        }
+
+
     }
 }
